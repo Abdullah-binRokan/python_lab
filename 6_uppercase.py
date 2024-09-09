8ccaf0f8467b726d65a7b0023ea485391152ecbb
AR <aarokan@gmail.com>
2024-09-09 21:00:37 +0300

    uppercase using str upper method and f string

--- 6_uppercase.py
+++ 6_uppercase.py
@@ -1,5 +1,3 @@
 before = input("Before: ")
-print("Afer:   ", end="")  # added the named parameters to alter default behavior
-for c in before:
-    print(c.upper(), end="")  # end="" will alter the default new line behaivor
-print()
+after = before.upper()
+print(f"Afer:   {after}")  
