0af43e6059018c32943aaa350c731fe92799888a
AR <aarokan@gmail.com>
2024-09-10 20:44:37 +0300

    score: linked list and appending

---
+++ 1_syntax_dataTypes.py
@@ -0,0 +1,12 @@
+############################
+#### list ####
+############################
+
+# linked list
+score = [72, 73, 33]
+
+score.append(34)
+
+average = sum(score) / len(score)
+print(f"average = {average}")
+
