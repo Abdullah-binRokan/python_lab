e7d84367b78bccf87fba3250f9ccb1f419f1ca14
AR <aarokan@gmail.com>
2024-09-14 21:43:31 +0300

    exit: using sys Lib

--- 12_exit.py
+++ 12_exit.py
@@ -3,6 +3,6 @@ import sys
 if len(sys.argv) != 2:
     print("Missing command-line argument")
     sys.exit(1)
-else:
-    print(f"hello, {sys.argv[1]}")
-    sys.exit(0)
+
+print(f"hello, {sys.argv[1]}")
+sys.exit(0)
